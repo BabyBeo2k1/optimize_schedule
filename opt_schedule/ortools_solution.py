178cd80a45cc96310f8ec6bc004b0b4b3028dfba
truong2710-cyber <truong.vq194198@sis.hust.edu.vn>
2023-02-06 00:03:46 +0700

    edit ortool code

--- opt_schedule/ortools_solution.py
+++ opt_schedule/ortools_solution.py
@@ -1,6 +1,7 @@
 from ortools.linear_solver import pywraplp
 from math import floor
 import numpy as np
+import os
 
 def create_data_model(path='test.txt'):
     """Create the data for the example."""
@@ -10,16 +11,18 @@ def create_data_model(path='test.txt'):
         # convert each line to integer and append to the list
     
         read_file= [[int(i)for i in line.strip().split(' ')] for line in lines]
+    
     read_file=np.array(read_file)
     data={}
+    data['num_prod'] = read_file[0][0]
     data['min_num'] = read_file[4]
     data['cost'] = read_file[1]
     data['total_cost'] = read_file[0][2]
     data['area_cost'] = read_file[2]
     data['area'] = read_file[0][1]
-    data['upper_bound'] = [read_file[0][1]/i for i in read_file[1]]
+    data['upper_bound'] = [floor(min([data['total_cost']/data['cost'][i], data['area']/data['area_cost'][i]])) for i in range(data['num_prod'])]  #[read_file[0][1]/i for i in read_file[1]]
     data['profit'] = read_file[3]
-    data['num_prod'] = read_file[0][0]
+    
 
     return data
 
@@ -61,7 +64,7 @@ def main():
     if status == pywraplp.Solver.OPTIMAL:
         print("Cost limit:", data['total_cost'])
         print("Area limit:", data['area'])
-        print('Total profit:', int(objective.Value()))
+        print('Total profit:', round(objective.Value()))
 
         for i in range(data['num_prod']):
             if x[i].solution_value() > 0:
